bd61af5baf2147c3a88eebc7e3b6cd77f868c6c8
Matheus Souza Jose <matheus.jose@aluno.faculdadeimpacta.com.br>
2023-07-17 19:32:55 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('jest').Config} */
+const config = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
+
+module.exports = config
