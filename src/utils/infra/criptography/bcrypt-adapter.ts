8f91789e2dfaeef1f50c465ba3b899cff97016f8
Matheus Souza Jose <matheus.jose@aluno.faculdadeimpacta.com.br>
2023-07-18 20:24:22 -0300

    feat: ensure BcryptAdapter calls bcrypt wuth correct values

---
+++ src/utils/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,11 @@
+import { type Encrypter } from '../../../data/protocols/encrypter'
+import bcrypt from 'bcrypt'
+
+export class BcryptAdapter implements Encrypter {
+  constructor (private readonly salt: number) {
+  }
+
+  async encrypt (value: string): Promise<string> {
+    return await bcrypt.hash(value, this.salt)
+  }
+}
