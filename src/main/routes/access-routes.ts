af4fe6c62100820a729d2ee44bae52490e6febcd
Matheus Souza Jose <matheus.jose@aluno.faculdadeimpacta.com.br>
2023-08-02 00:05:05 -0300

    refactor: add aliases type

--- src/main/routes/access-routes.ts
+++ src/main/routes/access-routes.ts
@@ -1,9 +1,9 @@
 import { type Router } from 'express'
-import { adaptRoute } from '../adapters/express-route-adapter'
-import { makeSignUpController } from '../factories/controllers/access/signup/signup-controller-factory'
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
+import { makeSignUpController } from '@/main/factories/controllers/access/signup/signup-controller-factory'
 import {
   makeSignInController
-} from '../factories/controllers/access/signin/signin-controller-factory'
+} from '@/main/factories/controllers/access/signin/signin-controller-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
