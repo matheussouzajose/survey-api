e09d6d1dce9321a89fb3cb7146a29b7ff251c0f9
Matheus Souza Jose <matheus.jose@aluno.faculdadeimpacta.com.br>
2023-08-06 00:15:55 -0300

    refactor: change auth middleware

--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -1,10 +1,10 @@
 import { type Router } from 'express'
-import { adminAuth } from '@/main/middlewares/admin-auth'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import {
   makeSaveSurveyResultController
 } from '@/main/factories/controllers/survey-result/save-surver-result/save-survey-result-controller-factory'
+import { auth } from '@/main/middlewares/auth'
 
 export default (router: Router): void => {
-  router.put('/surveys/:surveyId/results', adminAuth, adaptRoute(makeSaveSurveyResultController()))
+  router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
 }
